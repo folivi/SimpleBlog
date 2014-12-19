a56150ce95f8953192a50659447df7fef4ec4d55
Folivi FOLLY <ab.folivi@gmail.com>
2014-12-19 06:13:25 +0100

    cleaning up

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @articles = Article.published    
-    @hottest_topics = Article.order('created_at desc').published #derniers articles du plus réceent au plus ancien
-  end
-end
